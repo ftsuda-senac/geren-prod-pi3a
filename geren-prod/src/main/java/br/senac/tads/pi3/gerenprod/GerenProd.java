b25393b8b0c1142c64a52dc86b27bec577b9b834
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-03-01 21:59:13 -0300

    Operacao i++

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
@@ -13,6 +13,8 @@ public class GerenProd {
     
     public static void main(String[] args) {
         System.out.println("Teste Maven");
+        int i = 0;
+        System.out.println("Valor de i++: " + i++);
     }
     
 }
